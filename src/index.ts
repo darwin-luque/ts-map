b584f340abb07e7ace109d9466edb874422c64a5
Darwin Luque <darwin.luque.98@gmail.com>
2021-08-15 23:41:30 -0600

    add google maps functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+/// <reference types="@types/google.maps" />
 import { User } from './User';
 import { Company } from './Company';
 
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 3,
+  center: { lat: 0, lng: 0 },
+});
